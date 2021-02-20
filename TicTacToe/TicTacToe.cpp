ed2e8ad09ebfaa3ea76b6acf49558c0581ea0ea6
Max Elliott <breathe.max@gmail.com>
2021-02-20 15:27:34 -0800

    First commit of TicTacToe. No real code yet.

---
+++ TicTacToe/TicTacToe.cpp
@@ -0,0 +1,23 @@
+// This is an implementation of TicTacToe for Windows Console. This was written by Max Elliott as part of a programming test/assignment for 
+// Psyonix in February of 2021
+
+#include <iostream>
+
+
+// We will need a few things as part of the core architecture/functionality
+// 1) A data structure to represent the state of the board
+// 2) A method for checking the state of the board for win/lose/draw
+// 3) A way to display the current state of the board
+// 4) Some command/input parsing
+// 5) A help system
+// 6) Some documentation
+
+// BONUS POINTS:
+// 1) An undo system that lets the user rewind/forward the game arbitrarily
+// 2) Support a generalized m,n,k variation of the game 
+
+// The code should essentially be self documenting, but if I have time, I will add some simple HTML docs
+int main()
+{
+    std::cout << "Welcome to the TicTacToe Game!\n";
+}
